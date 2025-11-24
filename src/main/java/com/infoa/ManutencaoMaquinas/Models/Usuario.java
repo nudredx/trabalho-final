ba273a2f0cb5425db58bed7f0aaf013965ae2e1d
nudredx <luis.f.nunes6@aluno.senai.br>
2025-11-24 11:15:17 -0300

    Add files via upload

--- src/main/java/com/infoa/ManutencaoMaquinas/Models/Usuario.java
+++ src/main/java/com/infoa/ManutencaoMaquinas/Models/Usuario.java
@@ -17,14 +17,8 @@ public class Usuario {
      @Column(name="id")
     private Integer id;
 
-     @Column(name="cpf")
-    private String cpf;
-
      @Column(name="nome")
     private String nome;
-
-     @Column(name="datanascimento")
-    private LocalDate dataNascimento;
     
     @Column(name="email")
     private String email;
@@ -34,11 +28,9 @@ public class Usuario {
 
     public Usuario() {}
 
-    public Usuario(Integer id, String cpf, String nome, LocalDate dataNascimento, String email, String senha) {
+    public Usuario(Integer id,String nome,String email, String senha) {
         this.id = id;
-        this.cpf = cpf;
-        this.nome = nome;
-        this.dataNascimento = dataNascimento;
+        this.nome = nome; 
         this.email = email;
         this.senha = senha;
     }
@@ -51,13 +43,7 @@ public class Usuario {
         this.id = id;
     }
 
-    public String getCpf() {
-        return cpf;
-    }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
 
     public String getNome() {
         return nome;
@@ -67,13 +53,6 @@ public class Usuario {
         this.nome = nome;
     }
 
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
 
     public String getEmail() {
         return email;
